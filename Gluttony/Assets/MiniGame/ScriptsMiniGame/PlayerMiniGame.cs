16c5ce466546d04160dde23cf4b3eb297b8e63d8
Vlada Shudegova <vlada.shudegova@gmail.com>
2024-06-07 17:48:57 +0700

    Add method flip for sprite of player

--- Gluttony/Assets/MiniGame/ScriptsMiniGame/PlayerMiniGame.cs
+++ Gluttony/Assets/MiniGame/ScriptsMiniGame/PlayerMiniGame.cs
@@ -7,27 +7,32 @@ public class PlayerMiniGame : MonoBehaviour
 
     [SerializeField] float speed = 10f;
     Rigidbody2D rb2d; 
+    SpriteRenderer spriteRenderer;
     
     [SerializeField]   Animator anim; 
+
 
     void Awake(){
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         anim = GetComponent< Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
     }
-    void FixedUpdate(){
+    void Update(){
 
         //anim.SetTrigger("isRunMiniGame");
       
-       // float deltaX = speed * Input.GetAxis("Horizontal");
-        
-        //rb2d.AddForce(new Vector2(deltaX, 0) * speed);
-        //rb2d.velocity = new Vector2(deltaX, rb2d.velocity.y);
-
-        //rb2d.velocity = new Vector2(speed * input, rb2d.velocity.y);
-
         float deltaX = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        Flip();
         gameObject.transform.Translate(deltaX, 0, 0);
         
     }
-    
+
+    void Flip(){
+        if(Input.GetAxis("Horizontal") < 0) 
+            spriteRenderer.flipX = true; 
+        else if (Input.GetAxis("Horizontal") > 0)
+            spriteRenderer.flipX = false; 
+    }
+
 }
